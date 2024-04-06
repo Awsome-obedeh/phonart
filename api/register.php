94ed0c5a0aa41261644c1367d4e62eff4ca809f3
obed chidera <obedchidera1010@gmail.com>
2024-04-06 10:58:12 +0100

    validated register api

--- api/register.php
+++ api/register.php
@@ -10,18 +10,26 @@ $name=$data['name'];
 $number=$data['number'];
 $password=$data['password'];
 
-$insert_sql="INSERT INTO numbers(phone,name,password)
-VALUES('$number','$name','$password')";
-$insert_query=mysqli_query($conn,$insert_sql);
+if($name!='' && $number !='' && $password !=''){
 
-if($insert_query){
-    echo json_encode(array("status"=>200, "msg"=>"User registered successfully"));
-
+    $insert_sql="INSERT INTO numbers(phone,name,password)
+    VALUES('$number','$name','$password')";
+    $insert_query=mysqli_query($conn,$insert_sql);
+    
+    if($insert_query){
+        echo json_encode(array("status"=>200, "msg"=>"User registered successfully"));
+    
+    }
+    else{
+        echo json_encode(array("status"=>500, "msg"=>"server error"));
+        // echo mysqli_error($conn);
+    }
 }
+
 else{
-    echo json_encode(array("status"=>500, "msg"=>"server error"));
-    // echo mysqli_error($conn);
+    echo json_encode(array("status"=>400, "msg"=>"Missin user inputs"));
 }
+
 
 
 
