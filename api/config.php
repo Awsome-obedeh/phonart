178c67a22d7c474de57b2851bc526994f9a26e0f
obed chidera <obedchidera1010@gmail.com>
2024-04-26 08:50:16 +0100

    changes status code

--- api/config.php
+++ api/config.php
@@ -2,7 +2,7 @@
     function cors(){
         
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: *");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
     header("Access-Control-Allow-Headers: Authorization, Content-Type");
     }
 
--- api/login.php
+++ api/login.php
@@ -49,7 +49,7 @@ function jwt_payload($number,$id){
                 $id=$users['id'];
                 $number=$users['phone'];
                 $jwt=JWT::encode(jwt_payload($number,$id), $secret_key,'HS256' );
-                echo json_encode(array("status"=>"200","msg"=>"user logged in successfully", "token"=>$jwt));
+                echo json_encode(array("status"=>200,"msg"=>"user logged in successfully", "token"=>$jwt));
             }
     
     
